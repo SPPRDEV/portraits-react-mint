56b7b345587e6362aa0f0dab453ec9f776ecef9a
Diluk Angelo <dilukangelo@gmail.com>
2024-11-15 12:38:25 +0530

    initial

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { CrossmintPayButton } from "@crossmint/client-sdk-react-ui";
+
+export default function Mint() {
+  return (
+    <div className="w-full p-5 flex lg:flex-row flex-col-reverse items-center justify-between ">
+      <div className="flex flex-col gap-2 max-w-lg">
+        <div className="text-black text-2xl font-normal font-['Cardo']">
+          Minting.
+        </div>
+        <div className="w-full  text-black text-base font-normal font-['Avenir Next LT Pro'] leading-normal">
+          Sit sit eius culpa eligendi neque laborum ex doloremque ut. In
+          exercitationem ex dignissimos quaerat earum perspiciatis. Aliquam
+          laboriosam nam enim aliquid. Id sed et aut facilis. Architecto
+          possimus et et sed esse nulla consequatur.
+          <br />
+          <br />
+          Sit qui et natus dolore. Molestiae omnis explicabo sit nobis neque
+          sint voluptate. Consequatur ad sed. Corrupti eos excepturi dolores
+          porro.
+        </div>
+        <div className="flex flex-col gap-4 mt-3">
+        <CrossmintPayButton
+          collectionId="69b57982-b9d1-4ab7-8bc2-20eb2fbbdf87"
+          projectId="e22a833b-af6b-4d8c-9fe5-515d6c1f17a7"
+          mintConfig={{ totalPrice: "0.01", _amount: "1" }}
+          environment="staging"
+          checkoutProps={{ paymentMethods: ["ETH", "SOL"] }}
+        />
+        </div>
+      </div>
+      <video
+      autoPlay
+      loop
+      muted
+      className="lg:w-[400px] w-full"
+    >
+      <source src="/nft.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+       
+    </div>
+  );
+}
---
+++ src/AppKitProvider.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { createAppKit } from '@reown/appkit/react'
+
+import { WagmiProvider } from 'wagmi'
+import { sepolia} from '@reown/appkit/networks'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+
+// 0. Setup queryClient
+const queryClient = new QueryClient()
+
+// 1. Get projectId from https://cloud.reown.com
+const projectId = '6e1107fb5c550e76ebfa2981094dcba4'
+
+// 2. Create a metadata object - optional
+const metadata = {
+  name: 'MapNft',
+  description: 'AppKit Example',
+  url: 'https://reown.com/appkit', // origin must match your domain & subdomain
+  icons: ['https://assets.reown.com/reown-profile-pic.png']
+}
+
+// 3. Set the networks
+const networks:any = [sepolia]
+
+// 4. Create Wagmi Adapter
+const wagmiAdapter = new WagmiAdapter({
+  networks,
+  projectId,
+  ssr: true
+});
+
+// 5. Create modal
+createAppKit({
+  adapters: [wagmiAdapter],
+  networks,
+  projectId,
+  metadata,
+  features: {
+    analytics: true // Optional - defaults to your Cloud configuration
+  }
+})
+
+export function AppKitProvider({ children }:any) {
+  return (
+    <WagmiProvider config={wagmiAdapter.wagmiConfig}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+    
